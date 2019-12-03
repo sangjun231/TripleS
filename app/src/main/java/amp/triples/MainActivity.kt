8f354762b0c7087310d3a902cb917c23554ceaa4
nfamily0 <nfamily123@gmail.com>
2019-12-03 12:13:52 +0900

    Merge branch 'feature-ksb' of https://github.com/sangjun231/TripleS into son9son9
    # Conflicts:
    #	app/src/main/java/amp/triples/MainActivity.kt

--- app/src/main/java/amp/triples/MainActivity.kt
+++ app/src/main/java/amp/triples/MainActivity.kt
@@ -4,8 +4,13 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +20,8 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         //  splash image 시작
         startActivity<SplashActivity>()
@@ -50,6 +57,11 @@ class MainActivity : AppCompatActivity() {
             "1",
             "json"
         )
+
+        //test view update
+        val testSKYValue : String = "3"
+        viewUpdate(testSKYValue)
+
         val a = RestPullManager.url(0)
         Log.i("test", a)
 
@@ -85,5 +97,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun networkCheck() = (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo?.isConnected ?: false
+
+    //son9son9
+    private fun viewUpdate (testSKYValue: String) {
+        when (testSKYValue) {
+            "1" -> { statusImage1.setImageResource(R.drawable.weather_sunny)
+                        status.setText("맑음")}
+            "3" -> { statusImage1.setImageResource(R.drawable.weather_cloudandsun)
+                status.setText("구름 많음")}
+            "4" -> { statusImage1.setImageResource(R.drawable.weather_cloudy)
+                status.setText("흐림")}
+            else -> toast("SKY value is invalid")
+
+        }
+    }
 
 }
