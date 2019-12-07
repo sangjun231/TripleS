3f36639afedfbd4ad5e85904f9d9277050c300b7
Cling-A <dkgkgk249@gamail,com>
2019-12-08 05:33:23 +0900

    Add find dust activity

---
+++ app/src/main/java/amp/triples/FineDustActivity.kt
@@ -0,0 +1,14 @@
+package amp.triples
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FineDustActivity :  AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fine_dust)
+
+    }
+}
