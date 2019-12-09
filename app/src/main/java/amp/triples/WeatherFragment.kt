ece98fbefa8842794e34ef4e1f5832e35d6d4a00
mainlib990 <mainlib990@gmail.com>
2019-12-09 15:03:42 +0900

    날씨 VIEW 완성

--- app/src/main/java/amp/triples/WeatherFragment.kt
+++ app/src/main/java/amp/triples/WeatherFragment.kt
@@ -1,17 +1,18 @@
 package amp.triples
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_weather.view.*
-import org.jetbrains.anko.backgroundColor
+import java.time.DayOfWeek
+import java.time.LocalDateTime
 
 
 class WeatherFragment : Fragment() {
@@ -24,24 +25,38 @@ class WeatherFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_weather, container, false)
 
         val location = GpsTracker(MainActivity.instance).location
-        val gu = MainActivity.instance.getCurrentAddress(location.latitude, location.longitude).split(" ")[2]
+        val gu = MainActivity.instance.getCurrentAddress(
+            location.latitude,
+            location.longitude
+        ).split(" ")[2]
         val forecastSpaceData = MyData.parseData!![0]
         val forecastGrib = MyData.parseData!![2]
         val forecastTimeData = MyData.parseData!![3]
-        val nowSky = forecastTimeData.getJSONObject(DateTime.now().substring(0, 8)).getJSONObject(DateTime.now().substring(8)).getInt("SKY")
+        Log.i("test", forecastTimeData.toString())
         val temp = forecastGrib.getInt("T1H")
 
         //  위치 업데이트
         view.location.text = gu
 
         //  구름 업데이트
-        with (view.status) {
+        with(view.status) {
 
-            text = when (nowSky) {
+            text = when (forecastTimeData.getJSONObject(DateTime.now().substring(0, 8))
+                .getJSONObject(DateTime.now().substring(8)).getInt("PTY")) {
 
-                1 -> "맑음"
-                3 -> "구름많음"
-                else -> "흐림"
+                0 ->
+                    when (forecastTimeData.getJSONObject(DateTime.now().substring(0, 8))
+                        .getJSONObject(DateTime.now().substring(8)).getInt("SKY")) {
+
+                        1 -> "맑음"
+                        3 -> "구름많음"
+                        else -> "흐림"
+
+                    }
+
+                1, 2 -> "비"
+                3 -> "눈"
+                else -> "소나기"
 
             }
 
@@ -50,19 +65,19 @@ class WeatherFragment : Fragment() {
         view.temperature.text = "$temp°"
 
         //  하루간 날씨 표시
-        for (i in 1..7) {
+        for (i in 1..9) {
 
             val linearLayout = LinearLayout(MainActivity.instance)
 
             linearLayout.orientation = LinearLayout.VERTICAL
             linearLayout.layoutParams = LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                1f
+                150,
+                ViewGroup.LayoutParams.MATCH_PARENT
             )
+            linearLayout.gravity = Gravity.CENTER
 
             val timeUnitTime = TextView(MainActivity.instance)
-            timeUnitTime.text = "${DateTime.now2(i).substring(8, 10)}"
+            timeUnitTime.text = "${DateTime.now2(i).substring(8, 10)}시"
             timeUnitTime.layoutParams = LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
@@ -70,8 +85,53 @@ class WeatherFragment : Fragment() {
             timeUnitTime.gravity = Gravity.CENTER
             linearLayout.addView(timeUnitTime)
 
+            val timeUnitImage = ImageView(MainActivity.instance)
+            timeUnitImage.layoutParams = LinearLayout.LayoutParams(
+                100,
+                100
+            )
+
+            when (forecastSpaceData.getJSONObject(
+                DateTime.now2(i).substring(
+                    0,
+                    8
+                )
+            ).getJSONObject(
+                DateTime.now2(i).substring(8)
+            ).getInt("PTY")) {
+
+                0 ->
+                    when (forecastSpaceData.getJSONObject(
+                        DateTime.now2(i).substring(
+                            0,
+                            8
+                        )
+                    ).getJSONObject(DateTime.now2(i).substring(8)).getInt("SKY")) {
+
+                        1 -> timeUnitImage.setImageResource(R.drawable.weather_sunny)
+                        3 -> timeUnitImage.setImageResource(R.drawable.weather_cloudandsun)
+                        4 -> timeUnitImage.setImageResource(R.drawable.weather_cloudy)
+
+                    }
+
+                1, 2 -> timeUnitImage.setImageResource(R.drawable.weather_rain)
+                3 -> timeUnitImage.setImageResource(R.drawable.weather_snow)
+                4 -> timeUnitImage.setImageResource(R.drawable.weather_umbrella)
+
+            }
+
+            linearLayout.addView(timeUnitImage)
+
             val timeUnitTemp = TextView(MainActivity.instance)
-            timeUnitTemp.text = "${forecastSpaceData.getJSONObject(DateTime.now2(i).substring(0, 8)).getJSONObject(DateTime.now2(i).substring(8)).getInt("T3H")}°"
+            timeUnitTemp.text =
+                "${forecastSpaceData.getJSONObject(
+                    DateTime.now2(i).substring(
+                        0,
+                        8
+                    )
+                ).getJSONObject(
+                    DateTime.now2(i).substring(8)
+                ).getInt("T3H")}°"
             timeUnitTemp.layoutParams = LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
@@ -83,6 +143,23 @@ class WeatherFragment : Fragment() {
 
         }
 
+        with (view.dayOfTheWeek1) {
+
+            text = when (LocalDateTime.now().dayOfWeek) {
+
+                DayOfWeek.SUNDAY -> "일요일"
+                DayOfWeek.MONDAY -> "월요일"
+                DayOfWeek.TUESDAY -> "화요일"
+                DayOfWeek.WEDNESDAY -> "수요일"
+                DayOfWeek.THURSDAY -> "목요일"
+                DayOfWeek.FRIDAY -> "금요일"
+                DayOfWeek.SATURDAY -> "토요일"
+
+            }
+
+        }
+
         return view //super.onCreateView(inflater, container, savedInstanceState)
+
     }
 }
